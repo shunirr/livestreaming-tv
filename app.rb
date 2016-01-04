73c3d32f14e72559281ff1c818ce3763d372d33f
shunirr <m@s5r.jp>
2016-01-04 15:08:37 +0900

    Refactor, Update ffmpeg option.

--- app.rb
+++ app.rb
@@ -5,10 +5,14 @@ require 'sinatra'
 
 RECTEST_COMMAND = 'RecTest.exe'
 RECTEST_PATH    = 'C:\\tv\\TVTest'
+RECTEST_PORT    = 3456
 FFMPEG_COMMAND  = 'ffmpeg.exe'
 FFMPEG_PATH     = 'C:\\tv\\ffmpeg\\bin'
 WWW_PATH        = 'C:\\nginx\\html\\tv\\hls'
 M3U8_FILENAME   = 'playlist.m3u8'
+
+TS_FPS           = 24
+HLS_SEGMENT_TIME = 2
 
 $channel_id = nil
 $ffmpeg_thread = nil
@@ -24,9 +28,9 @@ end
 def start_rectest
   puts "start_rectest ch = #{$channel_id}"
   if $channel_id.nil?
-    system('start', "#{RECTEST_PATH}\\#{RECTEST_COMMAND}", '/udp')
+    system('start', "#{RECTEST_PATH}\\#{RECTEST_COMMAND}", '/udp', '/udpport', RECTEST_PORT.to_s)
   else
-    system('start', "#{RECTEST_PATH}\\#{RECTEST_COMMAND}", '/rch', $channel_id.to_s, '/udp')
+    system('start', "#{RECTEST_PATH}\\#{RECTEST_COMMAND}", '/rch', $channel_id.to_s, '/udp', '/udpport', RECTEST_PORT.to_s)
   end
 end
 
@@ -38,21 +42,22 @@ def start_ffmpeg
         File.delete f
       end
       Open3.popen3("#{FFMPEG_PATH}\\#{FFMPEG_COMMAND}",
-                   '-i', 'udp://127.0.0.1:3456?pkt_size=262144^&fifo_size=1000000^&overrun_nonfatal=1',
+                   '-i', "udp://127.0.0.1:#{RECTEST_PORT}?pkt_size=262144^&fifo_size=1000000^&overrun_nonfatal=1",
                    '-f', 'mpegts',
                    '-threads', 'auto',
                    '-map', '0:0', '-map', '0:1',
-                   '-acodec', 'libvo_aacenc', '-ar', '48000', '-ab', '192k', '-ac', '2',
+                   '-acodec', 'libvo_aacenc', '-ar', '44100', '-ab', '128k', '-ac', '2',
                    '-vcodec', 'libx264', '-s', '1280x720', '-aspect', '16:9', '-vb', '2m',
-                   '-g', '1',
+		   '-r', TS_FPS.to_s,
+                   '-g', "#{TS_FPS * HLS_SEGMENT_TIME}",
+		   '-force_key_frames', "expr:(t/#{HLS_SEGMENT_TIME})",
                    '-f', 'segment',
                    '-segment_format', 'mpegts',
-                   '-segment_time', '2',
+                   '-segment_time', HLS_SEGMENT_TIME.to_s,
                    '-segment_list', "#{WWW_PATH}\\#{M3U8_FILENAME}",
                    '-segment_list_flags', 'live',
                    '-segment_wrap', '50',
                    '-segment_list_size', '5',
-                   '-force_key_frames', 'expr:(t)',
                    '-break_non_keyframes', '1',
                    "#{WWW_PATH}\\stream%d.ts") do |i, o, e, w|
       end

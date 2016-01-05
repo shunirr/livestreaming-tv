3947b2e7f5723773d91d42ab26e78b2b97fcc0f4
shunirr <m@s5r.jp>
2016-01-05 17:56:26 +0900

    Reload

--- app.rb
+++ app.rb
@@ -18,11 +18,12 @@ module LiveStreamingTV
   class RecTest
     def initialize
       @pid = 0
+      @ch = 0
     end
 
     def restart(ch = 0)
+      @ch = ch
       stop
-      start ch
     end
 
     def stop
@@ -34,19 +35,22 @@ module LiveStreamingTV
 
     def start(ch = 0)
       return if @pid > 0
+      @ch = ch if ch > 0
 
       Thread.start do 
-        puts "start rectest (ch = #{ch})"
-        if ch > 0
-          Open3.popen3(RECTEST_PATH, '/rch', ch.to_s, '/udp', '/udpport', RECTEST_PORT.to_s) do |i, o, e, w|
-            @pid = w.pid
+        puts "start rectest (ch = #{@ch})"
+	loop {
+          if @ch > 0
+            Open3.popen3(RECTEST_PATH, '/rch', @ch.to_s, '/udp', '/udpport', RECTEST_PORT.to_s) do |i, o, e, w|
+              @pid = w.pid
+            end
+          else
+            Open3.popen3(RECTEST_PATH, '/udp', '/udpport', RECTEST_PORT.to_s) do |i, o, e, w|
+              @pid = w.pid
+            end
           end
-        else
-          Open3.popen3(RECTEST_PATH, '/udp', '/udpport', RECTEST_PORT.to_s) do |i, o, e, w|
-            @pid = w.pid
-          end
-        end
-        @pid = 0
+          @pid = 0
+	}
       end
     end
 
@@ -62,7 +66,6 @@ module LiveStreamingTV
 
     def restart
       stop
-      start
     end
 
     def stop
@@ -76,34 +79,36 @@ module LiveStreamingTV
       return if @pid > 0
 
       Thread.start do
-        puts "start ffmpeg"
-        delete_temp_files
-        now = Time.now.to_i
-        Open3.popen3(FFMPEG_PATH,
-                     '-i', "udp://127.0.0.1:#{RECTEST_PORT}?pkt_size=262144^&fifo_size=1000000^&overrun_nonfatal=1",
-                     '-f', 'mpegts',
-                     '-threads', 'auto',
-                     '-map', '0:0', '-map', '0:1',
-                     '-acodec', 'libvo_aacenc', '-ar', '44100', '-ab', '128k', '-ac', '2',
-                     '-vcodec', 'libx264', '-s', '1280x720', '-aspect', '16:9', '-vb', '2m',
-                     '-r', TS_FPS.to_s,
-                     '-g', "#{TS_FPS * HLS_SEGMENT_TIME}",
-                     '-force_key_frames', "expr:(t/#{HLS_SEGMENT_TIME})",
-                     '-f', 'segment',
-                     '-segment_format', 'mpegts',
-                     '-segment_time', HLS_SEGMENT_TIME.to_s,
-                     '-segment_list', "#{HLS_PATH}/#{now}_#{M3U8_FILENAME}",
-                     '-segment_list_flags', 'live',
-                     '-segment_wrap', '50',
-                     '-segment_list_size', '5',
-                     '-break_non_keyframes', '1',
-                     "#{HLS_PATH}/#{now}_stream%d.ts") do |i, o, e, w|
-                       puts "ffmpeg is running (pid = #{w.pid})"
-                       @pid = w.pid
-                       e.each {|l| puts l}
-                     end
-        puts "ffmpeg is dead"
-        @pid = 0
+        loop {
+          puts "start ffmpeg"
+          delete_temp_files
+          now = Time.now.to_i
+          Open3.popen3(FFMPEG_PATH,
+                       '-i', "udp://127.0.0.1:#{RECTEST_PORT}?pkt_size=262144^&fifo_size=1000000^&overrun_nonfatal=1",
+                       '-f', 'mpegts',
+                       '-threads', 'auto',
+                       '-map', '0:0', '-map', '0:1',
+                       '-acodec', 'libvo_aacenc', '-ar', '44100', '-ab', '128k', '-ac', '2',
+                       '-vcodec', 'libx264', '-s', '1280x720', '-aspect', '16:9', '-vb', '1m',
+                       '-r', TS_FPS.to_s,
+                       '-g', "#{TS_FPS}",
+                       '-force_key_frames', "expr:(t/#{HLS_SEGMENT_TIME})",
+                       '-f', 'segment',
+                       '-segment_format', 'mpegts',
+                       '-segment_time', HLS_SEGMENT_TIME.to_s,
+                       '-segment_list', "#{HLS_PATH}/#{now}_#{M3U8_FILENAME}",
+                       '-segment_list_flags', 'live',
+                       '-segment_wrap', '50',
+                       '-segment_list_size', '5',
+                       '-break_non_keyframes', '1',
+                       "#{HLS_PATH}/#{now}_stream%d.ts") do |i, o, e, w|
+                         puts "ffmpeg is running (pid = #{w.pid})"
+                         @pid = w.pid
+                         e.each {|l| puts l}
+                       end
+          puts "ffmpeg is dead"
+          @pid = 0
+        }
       end
     end
 
@@ -129,6 +134,7 @@ module LiveStreamingTV
     configure do
       set :rectest, RecTest.new
       set :ffmpeg, FFmpeg.new
+      set :sequence, Hash.new
       settings.rectest.start
       settings.ffmpeg.start
     end
@@ -141,29 +147,47 @@ module LiveStreamingTV
       playlists = []
 
       Dir.glob("#{HLS_PATH}/*.m3u8").sort do |a, b|
-        File.basename(a) <=> File.basename(b)
+        # File.basename(a) <=> File.basename(b)
+        File.basename(b) <=> File.basename(a)
       end.each do |file|
         time = File.basename(file).match(/^\d+/)[0].to_i
-        File.open(file, 'r') do |f|
-          playlists << {:time => time, :m3u8 => M3u8::Playlist.read(f.read)}
+	begin
+          File.open(file) do |f|
+            m3u8 = M3u8::Playlist.read(f.read)
+	    settings.sequence[File.basename(file)] = m3u8.sequence
+            playlists << {:time => time, :m3u8 => m3u8}
+	  end
+	rescue
+	  sleep 0.1
+	  retry
 	end
       end
+        
+      seq = settings.sequence.values.inject {|sum, n| sum + n}
 
       if playlists.size == 0
         404
-      elsif playlists.size == 1
-	playlists[0][:m3u8].sequence = playlists[0][:m3u8].sequence + playlists[0][:time]
-	playlists[0][:m3u8].to_s.gsub('#EXT-X-ENDLIST', '')
+      # elsif playlists.size == 1
       else
-        playlists[0][:m3u8].items.concat(playlists[1][:m3u8].items)
-        playlists[0][:m3u8].items.shift(playlists[1][:m3u8].items.size)
-	playlists[0][:m3u8].sequence = playlists[1][:m3u8].sequence + playlists[1][:time] + playlists[1][:m3u8].items.size
+	playlists[0][:m3u8].sequence = seq 
+	playlists[0][:m3u8].target = HLS_SEGMENT_TIME 
 	playlists[0][:m3u8].to_s.gsub('#EXT-X-ENDLIST', '')
+      # else
+      #   playlists[0][:m3u8].items.shift([playlists[0][:m3u8].items.size, playlists[1][:m3u8].items.size].min)
+      #   playlists[0][:m3u8].items.concat(playlists[1][:m3u8].items)
+      #   playlists[0][:m3u8].sequence = seq
+      #   playlists[0][:m3u8].target = HLS_SEGMENT_TIME 
+      #   playlists[0][:m3u8].to_s.gsub('#EXT-X-ENDLIST', '')
       end
     end
 
     get '/hls/*.ts' do |filename|
-      File.read(File.join('public', "#{filename}.ts"))
+      file = File.join('public', "#{filename}.ts")
+      if File.exist? file
+        File.read(file)
+      else
+	404
+      end
     end
 
     post '/select_channel' do

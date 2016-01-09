9bd7ba8cdd60b05e171eb74f259eefdbd4054c8d
shunirr <m@s5r.jp>
2016-01-09 15:27:19 +0900

    Refactor.

--- app.rb
+++ app.rb
@@ -1,14 +1,12 @@
 # Windows 
 
 require 'open3'
-require 'time'
 require 'json'
 require 'sinatra'
 require 'twitter'
 require 'tempfile'
 
-RECTEST_PATH    = 'C:/tv/TVTest/RecTest.exe'
-RECTEST_PORT    = 3456
+RECTEST_URL     = 'udp://127.0.0.1:3456'
 FFMPEG_PATH     = 'C:/tv/ffmpeg/bin/ffmpeg.exe'
 EPGDUMP_PATH    = 'C:/tv/epgdump/epgdump.exe'
 BONDRIVER_PATH  = 'C:/tv/TVTest/BonDriver_Spinel_PT-T0.dll'
@@ -28,116 +26,58 @@ module LiveStreamingTV
     end
   end
 
-  class RecTest
-    def initialize
-      @pid = 0
-      @ch = 0
-    end
-
-    def restart(ch = 0)
-      @ch = ch
-      stop
-    end
-
-    def stop
-      if @pid > 0
-        Process.kill('KILL', @pid)
-        @pid = 0
-      end
-    end
-
-    def start(ch = 0)
-      return if @pid > 0
-      @ch = ch if ch > 0
-
-      Thread.start do 
-        puts "start rectest (ch = #{@ch})"
-	loop {
-          if @ch > 0
-            Open3.popen3(RECTEST_PATH, '/d', BONDRIVER_PATH, '/rch', @ch.to_s, '/udp', '/udpport', RECTEST_PORT.to_s) do |i, o, e, w|
-              @pid = w.pid
-            end
-          else
-            Open3.popen3(RECTEST_PATH, '/d', BONDRIVER_PATH, '/udp', '/udpport', RECTEST_PORT.to_s) do |i, o, e, w|
-              @pid = w.pid
-            end
-          end
-          @pid = 0
-	}
-      end
-    end
-
-    def running?
-      @pid > 0
-    end
-  end
-
   class FFmpeg
     def initialize
       @pid = 0
     end
 
-    def restart
-      stop
-    end
-
     def stop
-      if @pid > 0
-        Process.kill('KILL', @pid)
-        @pid = 0
-      end
+      return if @pid <= 0
+      Process.kill('KILL', @pid)
+      @pid = 0
     end
 
     def start
       return if @pid > 0
 
       Thread.start do
-        loop {
+        loop do
           puts "start ffmpeg"
-          now = Time.now.to_i
           Open3.popen2e(FFMPEG_PATH,
-                       '-i', "udp://127.0.0.1:#{RECTEST_PORT}?pkt_size=262144^&fifo_size=1000000^&overrun_nonfatal=1",
-                       '-threads', 'auto',
-                       '-map', '0:0', '-map', '0:1',
-                       '-acodec', 'libfdk_aac', '-ar', '44100', '-ab', '128k', '-ac', '2',
-                       # '-vcodec', 'libx264', '-s', '1280x720', '-aspect', '16:9', '-vb', '1m',
-                       '-vcodec', 'libx264', '-s', '800x450', '-aspect', '16:9', '-vb', '1m',
-                       # '-vcodec', 'libx264', '-s', '640x360', '-aspect', '16:9', '-vb', '500k',
-                       '-vsync', '1', '-async', '50',
-                       '-r', "#{TS_FPS}",
-                       '-g', "#{TS_FPS}",
-                       '-force_key_frames', "expr:gte(t,n_forced*#{HLS_SEGMENT_TIME})",
-                       '-f', 'flv',
-                       'rtmp://127.0.0.1:1935/hls/stream') do |i, o, w|
-                         puts "ffmpeg is running (pid = #{w.pid})"
-                         @pid = w.pid
-                         o.each do |l|
-                           puts l
-                           stop if l.include? 'New audio stream'
-                           if l.include? 'speed='
-                             l.match /speed=([0-9.]+)x/ do |md|
-                               speed = md[1].to_f
-                               stop if speed < 0.8
-                             end
-                           end
-                         end
-                       end
-          puts "ffmpeg is dead"
+                        '-i', "#{RECTEST_URL}?pkt_size=262144^&fifo_size=1000000^&overrun_nonfatal=1",
+                        '-threads', 'auto',
+                        '-map', '0:0', '-map', '0:1',
+                        '-acodec', 'libfdk_aac', '-ar', '44100', '-ab', '128k', '-ac', '2',
+                        # '-vcodec', 'libx264', '-s', '1280x720', '-aspect', '16:9', '-vb', '1m',
+                        '-vcodec', 'libx264', '-s', '800x450', '-aspect', '16:9', '-vb', '1m',
+                        # '-vcodec', 'libx264', '-s', '640x360', '-aspect', '16:9', '-vb', '500k',
+                        '-vsync', '1', '-async', '50',
+                        '-r', "#{TS_FPS}",
+                        '-g', "#{TS_FPS}",
+                        '-force_key_frames', "expr:gte(t,n_forced*#{HLS_SEGMENT_TIME})",
+                        '-f', 'flv',
+                        'rtmp://127.0.0.1:1935/hls/stream') do |i, o, w|
+                          @pid = w.pid
+                          o.each do |l|
+                            puts l
+                            stop if l.include? 'New audio stream'
+                            if l.include? 'speed='
+                              l.match /speed=([0-9.]+)x/ do |md|
+                                speed = md[1].to_f
+                                stop if speed < 0.8
+                              end
+                            end
+                          end
+                        end
           @pid = 0
-        }
+        end
       end
-    end
-
-    def running?
-      @pid > 0
     end
   end
 
   class Controller < Sinatra::Base
     configure do
-      set :rectest, RecTest.new
       set :ffmpeg, FFmpeg.new
-      settings.rectest.start
       settings.ffmpeg.start
 
       client = Twitter::REST::Client.new do |config|
@@ -150,7 +90,7 @@ module LiveStreamingTV
     end
 
     get '/' do
-       File.read(File.join('public', 'index.html'))
+      File.read(File.join('public', 'index.html'))
     end
 
     get '/current_channel.json' do
@@ -160,9 +100,7 @@ module LiveStreamingTV
 
     post '/select_channel' do
       ch = params[:ch].to_i
-      puts "select ch = #{ch}"
-      BonDriverController.channel ch
-      # settings.ffmpeg.restart
+      BonDriverController.channel(ch)
       200
     end
 
@@ -170,11 +108,12 @@ module LiveStreamingTV
       img = params[:url].sub(/^data:image\/png;base64,/, '').unpack('m').first
       Tempfile.create("#{rand(256**16).to_s(16)}.png") do |png|
         open(png, 'wb') {|f| f.write img }
-	open(png) do |f|
+        open(png) do |f|
           settings.twitter_client.update_with_media(' ', f)
-	end
+        end
       end
       201
     end
   end
 end
+
